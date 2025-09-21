2eb90d5bbdf9bc17369a1ea4cf41dc5ae17950c0
nnthach <nguyenngocthach2301@gmail.com>
2025-09-21 13:08:19 +0700

    Auth layout

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>HomeScreen</Text>
+      </View>
+    </SafeAreaView>
   );
 }
--- app/(root)/(tabs)/station.tsx
+++ app/(root)/(tabs)/station.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function StationScreen() {
   return (
-    <View>
-      <Text>StationScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>StationScreen</Text>
+      </View>
+    </SafeAreaView>
   );
 }
